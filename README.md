b3bf53694c8fdf7aa5d12f3059c3929d91ab400b
Kouhei Sano <sano@buddying.jp>
2019-05-23 11:41:59 +0900

    commit

--- README.md
+++ README.md
@@ -2,20 +2,6 @@
 
 db slim application
 
-```bash
-composer install
-cp -ap config/.env.prod config/.env
-```
-
-```bash
-composer dump-autoload
-```
-
-```bash
-php vendor/bin/doctrine orm:convert:mapping --namespace="App\Entity\\" --from-database yml config/doctrine
-php vendor/bin/doctrine orm:generate-entities src/Entity/
-```
-
 local site
 
 `http://localhost/`
---
+++ htdocs/src/Entity/UserLogs.php
@@ -0,0 +1,57 @@
+<?php
+namespace App\Entity;
+
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * UserLogs
+ *
+ * @ORM\Table(name="user_logs", indexes={@ORM\Index(name="fk_user_logs1", columns={"user_id"})})
+ * @ORM\Entity
+ */
+class UserLogs
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="text", type="text", length=65535, nullable=false)
+     */
+    private $text;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="created_at", type="datetime", nullable=false)
+     */
+    private $createdAt;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="updated_at", type="datetime", nullable=false)
+     */
+    private $updatedAt;
+
+    /**
+     * @var \Users
+     *
+     * @ORM\ManyToOne(targetEntity="Users")
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="user_id", referencedColumnName="id")
+     * })
+     */
+    private $user;
+
+
+}
+
